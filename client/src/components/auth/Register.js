7e77c45dccc0427cfcbf39505b1e4e9d1c8307b7
Linjun Ouyang <lynjunoyang@gmail.com>
2019-09-28 11:50:59 -0700

    Implemented Alert Component and related action calls

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,8 +1,10 @@
 import React, { Fragment, useState } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-// import axios from 'axios';
+import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types';
 
-const Register = () => {
+const Register = ({ setAlert }) => {
   // formData: an object with all of field values. Like State
   // setFormData: an function that sets state
   const [formData, setFormData] = useState({
@@ -20,29 +22,9 @@ const Register = () => {
   const onSubmit = async e => {
     e.preventDefault();
     if (password !== password2) {
-      console.log('Passwords do not match');
+      setAlert('Passwords do not match', 'danger', 3000);
     } else {
       console.log('SUCCESS');
-      //   const newUser = {
-      //     name,
-      //     email,
-      //     password
-      //   };
-
-      //   try {
-      //     const config = {
-      //       headers: {
-      //         'Content-Type': 'Application/json'
-      //       }
-      //     };
-
-      //     //   const body = JSON.stringify(newUser);
-
-      //     const res = await axios.post('/api/users', newUser, config);
-      //     console.log(res.data);
-      //   } catch (err) {
-      //     console.error(err.response.data);
-      //   }
     }
   };
 
@@ -106,4 +88,11 @@ const Register = () => {
   );
 };
 
-export default Register;
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired
+};
+
+export default connect(
+  null,
+  { setAlert }
+)(Register);
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Register from '../auth/Register';
 
 const Navbar = () => {
   return (
