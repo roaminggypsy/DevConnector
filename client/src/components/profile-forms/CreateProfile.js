01a7cd7a18f8a7ea405c1a2c0f57e1060ddee124
Linjun Ouyang <lynjunoyang@gmail.com>
2019-09-30 23:13:33 -0700

    Created Create Profile Action

--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -1,8 +1,10 @@
 import React, { useState, Fragment } from 'react';
+import { Link, withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { createProfile } from '../../actions/profile';
 
-const CreateProfile = props => {
+const CreateProfile = ({ createProfile, history }) => {
   const [formData, setFormData] = useState({
     company: '',
     website: '',
@@ -38,6 +40,11 @@ const CreateProfile = props => {
   const onChange = e =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
 
+  const onSubmit = e => {
+    e.preventDefault();
+    createProfile(formData, history);
+  };
+
   return (
     <Fragment>
       <h1 className='large text-primary'>Create Your Profile</h1>
@@ -46,7 +53,7 @@ const CreateProfile = props => {
         profile stand out
       </p>
       <small>* = required field</small>
-      <form className='form'>
+      <form className='form' onSubmit={e => onSubmit(e)}>
         <div className='form-group'>
           <select name='status' value={status} onChange={e => onChange(e)}>
             <option value='0'>* Select Professional Status</option>
@@ -92,7 +99,7 @@ const CreateProfile = props => {
             type='text'
             placeholder='Location'
             name='location'
-            value={website}
+            value={location}
             onChange={e => onChange(e)}
           />
           <small className='form-text'>
@@ -186,7 +193,7 @@ const CreateProfile = props => {
                 type='text'
                 placeholder='Linkedin URL'
                 name='linkedin'
-                value='linkedin'
+                value={linkedin}
                 onChange={e => onChange(e)}
               />
             </div>
@@ -197,7 +204,7 @@ const CreateProfile = props => {
                 type='text'
                 placeholder='Instagram URL'
                 name='instagram'
-                value='instagram'
+                value={instagram}
                 onChange={e => onChange(e)}
               />
             </div>
@@ -213,6 +220,11 @@ const CreateProfile = props => {
   );
 };
 
-CreateProfile.propTypes = {};
+CreateProfile.propTypes = {
+  createProfile: PropTypes.func.isRequired
+};
 
-export default CreateProfile;
+export default connect(
+  null,
+  { createProfile }
+)(withRouter(CreateProfile));
