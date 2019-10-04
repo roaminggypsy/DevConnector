e2a813236b211d9b94866cf18d43a613d46cfd76
Linjun Ouyang <lynjunoyang@gmail.com>
2019-10-04 15:04:59 -0700

    Implemented adding comments

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import PostItem from '../posts/PostItem';
-import { getPost, getPosts } from '../../actions/post';
+import CommentForm from './CommentForm';
+import { getPost } from '../../actions/post';
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
   useEffect(() => {
@@ -18,6 +19,7 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
         Back To Posts
       </Link>
       <PostItem post={post} showActions={false}></PostItem>
+      <CommentForm postId={post._id} />
     </Fragment>
   );
 };
